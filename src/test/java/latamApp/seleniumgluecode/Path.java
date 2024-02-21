bd36668eba4c26ddcd54a30812439265b84aba1b
Andersson Viveros <32718332+Andersonmcu@users.noreply.github.com>
2024-02-20 21:00:03 -0500

    Update Path.java

--- src/test/java/latamApp/seleniumgluecode/Path.java
+++ src/test/java/latamApp/seleniumgluecode/Path.java
@@ -1,12 +1,6 @@
 package latamApp.seleniumgluecode;
 
-
-
 public class Path {
 	public static final TipoDriver NAVEGADOR 	 = TipoDriver.CHROME;
 	public static final String 	   URL 			 = "https://www.latamairlines.com/co/es";
 }
-
-
-
-
